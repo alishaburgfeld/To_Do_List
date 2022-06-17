631287f0f22738de35eb443c54c0f637678bab97
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-06-16 21:24:18 -0400

    added basic html

---
+++ todolist.js
@@ -0,0 +1,7 @@
+//add checked when the button is clicked. -- probably do this through CSS, not JS, right? as well as the strikethrough?
+
+// function addTask(event) {
+//     preventDefault();
+//     createElement("div")
+//     //parent
+// }
